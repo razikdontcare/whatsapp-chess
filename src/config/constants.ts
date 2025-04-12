cd6aae5d269c87b3817ffea97196647a283bd18d
razikdontcare <gamersia4@gmail.com>
2025-04-13 03:44:48 +0800

    fix: comment out unused TEMP_IMAGE_PATH and LICHESS_API_URL constants in constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
-export const TEMP_IMAGE_PATH = './temp_board.png';
-export const LICHESS_API_URL = 'https://lichess.org/api';
+// export const TEMP_IMAGE_PATH = './temp_board.png';
+// export const LICHESS_API_URL = 'https://lichess.org/api';
 export const STOCKFISH_LEVEL = 3;
 export const MOVE_TIME_LIMIT = 30 * 1000; // 30 seconds per move
 export const BOARD_IMAGE_QUALITY = 80;
