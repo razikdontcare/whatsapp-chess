be12c0d72986cecc7f8f15c842731eea84f05a74
razikdontcare <gamersia4@gmail.com>
2025-04-13 03:41:02 +0800

    fix: comment out unused StockfishService import and related code in ChessBot

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { WhatsAppService } from "./services/whatsappService.js";
-import { StockfishService } from "./services/stockfishService.js";
+// import { StockfishService } from "./services/stockfishService.js";
 
 class ChessBot {
   private whatsappService: WhatsAppService;
-  private stockfishService: StockfishService;
+  //   private stockfishService: StockfishService;
 
   constructor() {
     this.whatsappService = new WhatsAppService();
-    this.stockfishService = new StockfishService();
+    // this.stockfishService = new StockfishService();
   }
 
   async start() {
-    await this.stockfishService.init();
+    // await this.stockfishService.init();
     await this.whatsappService.initialize();
     console.log("Chess Bot is running...");
   }
